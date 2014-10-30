7a5627ff9b9a4858251bc1e639e3f01f184d5916
Claus Aranha <caranha@cs.tsukuba.ac.jp>
2014-10-30 19:06:49 +0900

    open file and recognizes FNET or JMA

--- code/earthquake/catalog.py
+++ code/earthquake/catalog.py
@@ -6,10 +6,22 @@ Year, Month, Day, Hour, Minute, Second, Latitude, Longitude,
 Magnitude, Depth
 """
 
+# fnet has 19 columns, jma has 10 columns
 def readFromFile(filename):
     """ Returns a catalog created from a JMA or FNET text file
     """
-    pass
+    f = open(filename,"r")
+
+    # first, detecting JMA or FNET
+    for line in f:
+        if line[0] != '#':
+            tokens = line.split()
+            if (len(tokens)==10):
+                print ("JMA")
+            else:
+                print ("FNET")
+            return
+
 
 def filter(catalog,filter):
     """ Reduces a catalog by removing events that do not match the filter
