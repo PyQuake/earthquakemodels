be2c5e3e87213d3f9a9837ff30ba2d455fd436e1
ＣＯＳＳＩＣＨ　ＬＡＶＩＮＡＳ <s1455646@LC2RR-p044.u.tsukuba.ac.jp>
2014-11-04 12:41:07 +0900

    Changed the catalog file to inclued the data read in a list
    Im not very sure if what I did is what it should be done.
    Added the names of the columns is going to be important in the future, the info is the first element

--- code/earthquake/catalog.py
+++ code/earthquake/catalog.py
@@ -6,24 +6,39 @@ Year, Month, Day, Hour, Minute, Second, Latitude, Longitude,
 Magnitude, Depth
 """
 
+#Implementantion notes:
+# earthquakes is a list that contains the earthquake attributes
+#I dont know if the names of the columns is going to be important in the future, so I added this info in the first element
+
 # fnet has 19 columns, jma has 10 columns
 def readFromFile(filename):
     """ Returns a catalog created from a JMA or FNET text file
     """
     f = open(filename,"r")
 
+    earthquakes_JMA=['Lon Lat Year Month Day Mag_JMA Depth Hour Min Sec']
+    earthquakes_FNET=['Year Month Day Hour Min Sec Lat Lon Depth Mag_JMA S1 S2 D1 D2 R1 R2 Depth Mag_FNET Var']
+
     # first, detecting JMA or FNET
     for line in f:
         if line[0] != '#':
             tokens = line.split()
             if (len(tokens)==10):
-                print ("JMA")
+                #print ("JMA")
+		del earthquakes_FNET[:]
+		earthquakes_JMA.append(tokens)
             else:
-                print ("FNET")
-            return
+                #print ("FNET")
+		del earthquakes_JMA[:]
+		earthquakes_FNET.append(tokens)
+
+    if earthquakes_JMA != []:
+         return earthquakes_JMA
+    else:
+         return earthquakes_JNET
 
 
 def filter(catalog,filter):
     """ Reduces a catalog by removing events that do not match the filter
     """
-    pass
+    pass	
