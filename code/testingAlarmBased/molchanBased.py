c974f7659e0eb4c527fee4831bfb72c0cb079f38
yclavinas <yclavinas@gmail.com>
2014-12-26 18:37:09 +0900

    FInished the assTest function.
    There is some bug in its calculation, the results makes no much sense

--- code/testingAlarmBased/molchanBased.py
+++ code/testingAlarmBased/molchanBased.py
@@ -8,94 +8,102 @@ import models.mathUtil as mathUtil
 #they sould have the same len
 
 #Arguments are their bins, should it be changed???
-def molchan(modelLambda, modelOmega, catalog):
+def molchan(modelLambda, modelOmega):
 
-    if(len(modelLambda)==len(modelOmega)==len(catalog)):
+    if len(modelLambda.bins)==len(modelOmega.bins):
     	#TODO: Check how this normalize is done
     	referenceValues=[]
-    	referenceValues=mathUtil.normalize(modelOmega)
+    	referenceValues[:]=[x+1 for x in modelOmega.bins]
+    	
+    	referenceValues=mathUtil.normalize(referenceValues)
 		#TODO: e so somar os bins do Omega? Uma vez que a gente quer testar o modelo nosso contra os dados reais?
-    	N = sum(catalog)
+    	N = sum(modelOmega.bins)
     	#o que o claus tinha comentado aqui?????????
     	#tem tamanho N+1 no zechar, aqui como fica?
-    	trajectory=[]
-
+    	trajectory=[0]*(N+1)
 
     	#I need to order the in decresing order the values of modelOmega
 	    #I also need to have the same positions in the others, so thats what the zip is for
     	fullData=[]
-    	for value, lam, cat in zip(referenceValues, modelLambda, catalog):
-    		fullData.append((value,lam,cat))
+    	for lam,value in zip(modelLambda.bins, referenceValues):
+    		fullData.append((lam, value))
     	fullData.sort()
     	fullData.reverse()
 
-    	#Sera que vale a pena separar um do outro?
+    	testingValues=[]
+    	referenceValues=[]
+    	for data in fullData:
+    		testingValues.append(data[0])
+    		referenceValues.append(data[1])
+
     	hits=0
-    	for i in range(len(fullData)):
-    		tau=fullData[i][0]
-    		hitsInThisBin=fullData[i][2]
+    	tau=0
+    	for i in range(len(modelOmega.bins)):
+    		tau+=referenceValues[i]
+    		hitsInThisBin=0
+    		hitsInThisBin=modelOmega.bins[i]
     		if hitsInThisBin>0:
-    			thresholdInThisBin=fullData[i][1]
-    			thresholdInNextBin=fullData[min(i+1,len(fullData)-1)][1]
-    			while thresholdInThisBin==thresholdInNextBin and i<=len(fullData)-2:
+    			thresholdInThisBin=testingValues[i]
+    			thresholdInNextBin=fullData[min(i+1,len(testingValues)-1)]
+    			while thresholdInThisBin==thresholdInNextBin and i<=len(testingValues)-2:
     				i+=1
-    				tau+=fullData[i][0]
-    				hitsInThisBin+=fullData[i][2]
-    				if i<len(fullData)-1:
-    					thresholdInNextBin=fullData[i+1][1]
+    				tau+=referenceValues[i]
+    				hitsInThisBin+=modelOmega.bins[i]
+    				if i<(len(testingValues)-1):
+    					thresholdInNextBin=testingValues[i+1]
     				else:
     					thresholdInNextBin=float('-Infinity')
-
     			for j in range(hitsInThisBin):
-    				trajectory.insert(hits+j+1,tau)
+    				trajectory[hits+j+1]=tau
     			hits+=hitsInThisBin
     			if hits==N:
     				break
     	return trajectory
 
+def whichLegAreWeOn(molchanTrajectory, tau):
+	
+	n=0
+	for i in range(len(molchanTrajectory)):
+		if molchanTrajectory[i]<tau:
+			n=i
+		else:
+			return(i-1)
+	return n
+
 
 def areaUnderTrajectory(molchanTrajectory, tau):
 
-    n=0
-    i=1
-
-    while(i<len(molchanTrajectory)):
-    	if molchanTrajectory[i]<tau:
-    		n=i
-    	else:
-    		n=i-1
-    		break
-    	i+=1
+    n=whichLegAreWeOn(molchanTrajectory, tau)
 
     N = len(molchanTrajectory)-1
     height=[]
 
-    for i in range(N+1):
-    	height.append(N-i/N)
+    for i in range(len(molchanTrajectory)):
+    	height.append((N-i)/N)
 
     area=0
     for i in range(n-1):
     	area+=height[i]*(molchanTrajectory[i+1]-molchanTrajectory[i])
     area+=height[n]*(tau - molchanTrajectory[n])
-
+    
     area /= tau
+
     return area
     
 
-def assTest(modelLambda, modelOmega, catalog, tauSteps=0.01):
+def assTest(modelLambda, modelOmega, tauSteps=0.01):
 	"""
 	Calculates the ASS alarm test function defined by Zechar. 
 	Its as alarm based test that considers regions, threshold, miss rate and hits.
 	The param should be an model with bins that divides one region, and every bin should contain the quatity of earthquakes
 	"""
-	molchanTrajectory=molchan(modelLambda, modelOmega, catalog)
-	print(len(molchanTrajectory))
+	molchanTrajectory=molchan(modelLambda, modelOmega)
 	assTrajectory=[]
 
 	limitRange=int(1/tauSteps)
 	for i in range(limitRange):
 		tau=(i+1)*tauSteps
 		ass=1-areaUnderTrajectory(molchanTrajectory, tau)
-		assTrajectory.insert(i,ass)
+		assTrajectory.append(ass)
 
 	return assTrajectory
