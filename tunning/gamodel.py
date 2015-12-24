de4af0bf6e5b9da504c07cf4fda9976a5d5ff278
lara08 <lara08@lara08-HP-Compaq-8200-Elite-SFF-PC.(none)>
2015-12-24 09:27:50 -0200

    more machines working

--- tunning/gamodel.py
+++ tunning/gamodel.py
@@ -4,7 +4,7 @@ import pysmac
 import sys
 
 # sys.path.append('/Users/yclavinas/Documents/estudos/unb/earthquakemodels/code')
-sys.path.append('/home/lara03/Documents/earthquakemodels/code')
+sys.path.append('/home/lara08/Documents/earthquakemodels/code')
 
 import gaModel.gaModel_Yuri as etasGaModelNP
 import models.model as etasGa
@@ -29,7 +29,7 @@ def teste_ga(n_generations,crossover,mutation,inds):
 	observations=list()
 	qntYears = 5
 	region="Kanto"
-	year=2000
+	year=2001
 	for i in range(qntYears):
 		observation=etasGa.loadModelFromFile('../Zona/'+region+'real'+str(year+i)+'.txt')
 		observation.bins=observation.bins.tolist()
