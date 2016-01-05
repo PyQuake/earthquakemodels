aae064a613d37f258a95c9d5cbdbe5a0b9529ed1
lara04 <lara04@lara04pc.(none)>
2016-01-05 09:17:15 -0200

    only 2006 kanto

--- tunning/gamodel.py
+++ tunning/gamodel.py
@@ -4,7 +4,7 @@ import pysmac
 import sys
 
 # sys.path.append('/Users/yclavinas/Documents/estudos/unb/earthquakemodels/code')
-sys.path.append('/home/lara05/Documents/earthquakemodels/code')
+sys.path.append('/home/lara04/Documents/earthquakemodels/code')
 
 import gaModel.gaModel_Yuri as etasGaModelNP
 import models.model as etasGa
@@ -28,7 +28,7 @@ import models.model as etasGa
 def teste_ga(n_generations,crossover,mutation):
 	observations=list()
 	qntYears = 5
-	region="Kansai"
+	region="Kanto"
 	year=2006
 	for i in range(qntYears):
 		observation=etasGa.loadModelFromFile('../Zona/'+region+'real'+str(year+i)+'.txt')
