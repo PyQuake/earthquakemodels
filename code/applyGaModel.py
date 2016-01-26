008af58c7b229ccd91756a03e4406dc1b4e68fb0
lara03 <lara03@lara03pc.(none)>
2016-01-26 11:11:28 -0200

    3.0 exp

--- code/applyGaModel.py
+++ code/applyGaModel.py
@@ -126,8 +126,8 @@ def main():
 		# execGaModel(year, "Tohoku",save=True)
 		#execEtasGaModel(2005, "Kanto", save=True)
 		#execEtasGaModel(2005, "Kansai", save=True)
-		#execEtasGaModel(2005, "EastJapan", save=True)
-		execEtasGaModel(2005, "Tohoku", save=True)
+		execEtasGaModel(2005, "EastJapan", save=True)
+		#execEtasGaModel(2005, "Tohoku", save=True)
 
 
 if __name__ == "__main__":
