6aafdaf66b5b482e5686916a45e4a758535f4f17
yclavinas <yclavinas@gmail.com>
2016-05-05 15:49:27 -0300

    found a bug

--- code/addLL.py
+++ code/addLL.py
@@ -95,37 +95,29 @@ import csep.loglikelihood
 # 	year+=1
 
 
-def addLoglike2(region, depth, year_begin, year_end):
+def addLoglike2(type, region, depth, year_begin, year_end):
 
 	year=year_begin
 	while(year<=year_end):
 		print(year)
 		for i in range(10):
-			modelL=etasGa.loadModelFromFile('../Zona2/gaModel/'+region+'_'+str(depth)+'_'+str(year)+str(i)+'.txt')
+			modelL=etasGa.loadModelFromFile('../Zona2/'+type+'/'+region+'_'+str(depth)+'_'+str(year)+str(i)+'.txt')
 			modelO=observation=model.loadModelFromFile('../Zona2/realData/3.0'+region+'real'+str(depth)+"_"+str(year)+'.txt')
 			loglikeValue=csep.loglikelihood.calcLogLikelihood(modelL,modelO)
-			with open('../Zona2/gaModel/'+region+'_'+str(depth)+'_'+str(year)+str(i)+'.txtloglikelihood.txt', "w") as myfile:
+			with open('../Zona2/'+type+'/'+region+'_'+str(depth)+'_'+str(year)+str(i)+'.txtloglikelihood.txt', "w") as myfile:
 					myfile.write(str(loglikeValue))
 					myfile.write("\n")
 		year+=1
 
 def main():
-	#the year here already is the target year
-	addLoglike2('Tohoku', 25, 2005, 2010)
-	addLoglike2('Tohoku', 60, 2005, 2010)
-	addLoglike2('Tohoku', 100, 2005, 2010)
-
-	addLoglike2('EastJapan', 25, 2005, 2010)
-	addLoglike2('EastJapan', 60, 2005, 2010)
-	addLoglike2('EastJapan', 100, 2005, 2010)
-
-	addLoglike2('Kanto', 25, 2005, 2010)
-	addLoglike2('Kanto', 60, 2005, 2010)
-	addLoglike2('Kanto', 100, 2005, 2010)
-
-	addLoglike2('Kansai', 25, 2005, 2010)
-	addLoglike2('Kansai', 60, 2005, 2010)
-	addLoglike2('Kansai', 100, 2005, 2010)
+	types = ('listaGA_New', 'gaModel')
+	regions = ('EastJapan', 'Kansai', 'Kanto', 'Tohoku')
+	depths = (25, 60, 100)
+	for t in types:
+		for region in regions:
+			for depth in depths:
+		#the year here already is the target year
+				addLoglike2(t, region, depth, 2005, 2010)
 
 if __name__ == "__main__":
 	main()
