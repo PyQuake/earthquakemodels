6ea35be1c51fe318d4ea1535d1cb656d0345000e
lara03 <lara03@lara03pc.(none)>
2016-06-23 14:29:07 -0300

    kanto

--- code/filtredGAModel.py
+++ code/filtredGAModel.py
@@ -34,7 +34,7 @@ def execEtasGaModelClustered(year, region, depth, qntYears=5, times=30, save=Tru
 def main():
 	depth=25
 	year=2000
-	region = input()
+	region = 'Kanto'
 	while(year<2011):
 		# regions = ('Tohoku' ,'EastJapan', 'Kansai', 'Kanto')
 		# for region in regions:
