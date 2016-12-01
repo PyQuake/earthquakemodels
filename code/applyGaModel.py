36c31a93bab778e7b2aff5a5c1b269504dd10127
yclavinas <yclavinas@gmail.com>
2016-12-01 10:32:00 -0200

    exp: avr in  tohoku, eastjapan and kanto

--- code/applyGaModel.py
+++ code/applyGaModel.py
@@ -434,19 +434,20 @@ def execEtasGaModelSC(year, region, depth, qntYears=5, times=10, save=True):
 def main():
 		
 	# #exec models
-	region = 'Kanto'
+	regions = ('EastJapan', 'Kansai', 'Tohoku')
 	year=2000
 	depth = 100
 	# execGaModelSC(year, region, depth, save=False)
 	# createRealModelSC(year, region, depth, save=True)
 	# 
-	while(year<=2005):
-	# execParallelListGARandomParSC(year, region, depth=depth, save=False)
-	# execParallelGARandomParSC(year, region, depth=depth, save=False)
-#		execGaModel(year, region, depth=depth, save=True)
-		createandExecRealModelSCwithP_AVR(year, region, save = True)
-	# execParallelListGARandomPar(year, region, depth=depth, save=False)
-		year+=1
+	for region in regions:
+		while(year<=2005):
+		# execParallelListGARandomParSC(year, region, depth=depth, save=False)
+		# execParallelGARandomParSC(year, region, depth=depth, save=False)
+	#		execGaModel(year, region, depth=depth, save=True)
+			createandExecRealModelSCwithP_AVR(year, region, save = True)
+		# execParallelListGARandomPar(year, region, depth=depth, save=False)
+			year+=1
 
 if __name__ == "__main__":
 	main()
