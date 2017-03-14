c9ea8e98d6a1c0f198a2d782d800a658c1bf8305
root <root@cluster0.local>
2017-03-14 11:42:09 -0300

    exp

--- code/runExperiments/applyGaModel.py
+++ code/runExperiments/applyGaModel.py
@@ -54,8 +54,8 @@ def main():
     for the regions: EastJapan, Kanto, Kansai, Tohoku
     from 2000 to 2005 to create models from 2005 to 2010
     """
-    # region = 'Kanto'
-    # callGAModel(region)
+    region = 'Kanto'
+    callGAModel(region)
 
     # region = 'EastJapan'
     # callGAModel(region)
@@ -63,8 +63,8 @@ def main():
     # region = 'Tohoku'
     # callGAModel(region)
 
-    region = 'Kansai'
-    callGAModel(region)
+    # region = 'Kansai'
+    # callGAModel(region)
 
 
 
