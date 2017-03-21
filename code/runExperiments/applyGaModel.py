7b57a29dfd0839dd821abdf1cfc8422b510d3ca5
yclavinas <yclavinas@gmail.com>
2017-03-21 17:46:13 -0300

    cache exp

--- code/runExperiments/applyGaModel.py
+++ code/runExperiments/applyGaModel.py
@@ -33,7 +33,7 @@ def execGaModel(year, region, qntYears=5, times=1):
         model_.year=year+qntYears
         model_.modelName = region+'GAModel' 
         gaModel_ = model.loadModelDB(region+'GAModel', year)
-        if (gaModel_.definitions==None):    
+        # if (gaModel_.definitions==None):    
            # model.saveModelDB(model_)
            # model.saveModelToFile(observation,
            #  '../../Zona4/GAModel' + region +'GAModel' + str(year) + '_' + str(i) + '.txt')
