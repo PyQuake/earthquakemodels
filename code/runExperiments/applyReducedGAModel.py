62db1b31d917183dd06d6eafb28fa3990bf4ce50
yclavinas <yclavinas@gmail.com>
2017-04-22 13:44:31 -0300

    sel roullete both ga and reduced

--- code/runExperiments/applyReducedGAModel.py
+++ code/runExperiments/applyReducedGAModel.py
@@ -36,12 +36,12 @@ def execReducedGAModel(year, region, qntYears=5, times=10):
         # if (reducedGA_.definitions==None):    
     #     model.saveModelDB(model_)
         model.saveModelToFile(model_,
-            '../../Zona4/rawdata/ReducedGAModel/selRoulette' + region +'ReducedGAModel' + str(year+qntYears) + '_' + str(i) + '.txt')
-        with open("../../Zona4/ReducedGAModel/selRoulette" + region +"ReducedGAModel" + str(year+qntYears) + "_loglikelihood.txt", 'a') as f:
+            '..//rawdata/ReducedGAModel/selRoulette' + region +'ReducedGAModel' + str(year+qntYears) + '_' + str(i) + '.txt')
+        with open("..//ReducedGAModel/selRoulette" + region +"ReducedGAModel" + str(year+qntYears) + "_loglikelihood.txt", 'a') as f:
             f.write(str(model_.loglikelihood))
             f.write("\n")
             f.close() 
-        with open("../../Zona4/rawdata/ReducedGAModel/selRoulette" + region +"ReducedGAModel" + str(year+qntYears) + '_' + str(i) + "logbook.txt", 'w') as f:
+        with open("../rawdata/ReducedGAModel/selRoulette" + region +"ReducedGAModel" + str(year+qntYears) + '_' + str(i) + "logbook.txt", 'w') as f:
             f.write(str(model_.logbook))
             f.write("\n")
             f.close()    
