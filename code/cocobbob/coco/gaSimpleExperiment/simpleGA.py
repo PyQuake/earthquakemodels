c19d099f731244b2a3498a079c94137bfba92ac6
yclavinas <yclavinas@gmail.com>
2017-05-04 16:41:30 -0300

    based on  RCGA-P

--- code/cocobbob/coco/gaSimpleExperiment/simpleGA.py
+++ code/cocobbob/coco/gaSimpleExperiment/simpleGA.py
@@ -22,21 +22,18 @@ toolbox = base.Toolbox()
 creator.create("FitnessFunction", base.Fitness, weights=(-1.0,))
 creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessFunction)
 
-def gaModel(fun, problem_dimension, CXPB=0.9,MUTPB=0.1):
+def gaModel(fun, problem_dimension, CXPB=0.8,MUTPB=0.15):
 
 	#calculating the number of individuals of the populations based on the number of executions
-	fmax = 100000 * problem_dimension
-	n = int(numpy.sqrt(fmax) * 5)
-	NGEN = fmax / n
-	if fmax % n != 0:
-		NGEN += 1
-	
-	tournsize = n/500
-
+	fmax = 50000 * problem_dimension
+	n = min(100, 10 * problem_dimension)
+	# n = int(numpy.sqrt(fmax) * 5)
+	tournsize = 3
 
 	# Attribute generator
 	toolbox.register("attr_float", random.uniform, -5,5)
-	toolbox.register("mate", tools.cxOnePoint)
+	toolbox.register("mate", tools.cxBlend, alpha)
+	# toolbox.register("mate", tools.cxTwoPoint)
 	# operator for selecting individuals for breeding the next
 	# generation: each individual of the current generation
 	# is replaced by the 'fittest' (best) of three individuals
@@ -57,7 +54,6 @@ def gaModel(fun, problem_dimension, CXPB=0.9,MUTPB=0.1):
 	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 	logbook = tools.Logbook()
-	logbook.header = "gen","min","avg","max","std"
 
 	pop = toolbox.population(n)
 	# Evaluate the entire population
@@ -65,21 +61,23 @@ def gaModel(fun, problem_dimension, CXPB=0.9,MUTPB=0.1):
 	for ind, fit in zip(pop, fitnesses):
 		ind.fitness.values = fit
 
-	for g in range(NGEN):
+	for g in range(fmax):
 		# Select the next generation individuals
 		offspring = toolbox.select(pop, len(pop))
 		#create offspring
 		offspring = list(toolbox.map(toolbox.clone, pop))
 		# Apply crossover and mutation on the offspring
 		for child1, child2 in zip(offspring[::2], offspring[1::2]):
-			if random.random() < CXPB:
+			if random.random() > CXPB:
 				toolbox.mate(child1, child2)
 				del child1.fitness.values
 				del child2.fitness.values
-		for mutant in offspring:
-			if random.random() < MUTPB:
-				toolbox.mutate(mutant)
-				del mutant.fitness.values
+		# for mutant in offspring:
+				if random.random() < MUTPB:
+					toolbox.mutate(child1)
+				if random.random() < MUTPB:
+					toolbox.mutate(child2)
+				# del mutant.fitness.values
         # Evaluate the individuals with an invalid fitness
         
 		invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
@@ -99,6 +97,14 @@ def gaModel(fun, problem_dimension, CXPB=0.9,MUTPB=0.1):
 		for ind, fit in zip(pop, fitnesses):
 			ind.fitness.values = fit
 		record = stats.compile(pop)
+		if record.std < 1e-12:	
+			sortedPop = sorted(pop, key=attrgetter("fitness"), reverse = True)
+			pop = toolbox.population(n)
+			pop[0:fmax * 0.1] = sortedPop[0:fmax * 0.1]
+			fitnesses = list(toolbox.map(toolbox.evaluate, pop))
+			for ind, fit in zip(pop, fitnesses):
+				ind.fitness.values = fit
+
 		logbook.record(gen=g, **record)
 
 	return best_pop
