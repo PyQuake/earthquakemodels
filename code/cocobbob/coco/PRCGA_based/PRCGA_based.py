b8681f47f4b258d2525f2fb174cdfe2d71d92735
yclavinas <yclavinas@gmail.com>
2017-05-09 19:49:10 -0300

    trying to add pop and ind to deapbbob example file

--- code/cocobbob/coco/PRCGA_based/PRCGA_based.py
+++ code/cocobbob/coco/PRCGA_based/PRCGA_based.py
@@ -78,7 +78,7 @@ def main(func, dim, maxfuncevals, ftarget=None):
     fitnesses = list(toolbox.map(toolbox.evaluate, pop))
     for ind, fit in zip(pop, fitnesses):
     	ind.fitness.values = fit
-	maxfunevals - len(pop)
+	maxfuncevals - len(pop)
 	print(pop)
 	exit()
 	#end my code
