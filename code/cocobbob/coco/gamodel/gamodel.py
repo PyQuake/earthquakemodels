b718e32db303973c6970179a76f2dd2c86dc7df6
yclavinas <yclavinas@gmail.com>
2017-05-11 18:43:27 -0300

    checking how to get results

--- code/cocobbob/coco/gamodel/gamodel.py
+++ code/cocobbob/coco/gamodel/gamodel.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
 	for dim in (2, 3, 5, 10, 20, 40):
 		# Set the maximum number function evaluation granted to the algorithm
 		# This is usually function of the dimensionality of the problem
-		maxfuncevals = 10e2 * dim
+		maxfuncevals = 10e1 * dim
 		minfuncevals = dim + 2
 
 		# Iterate over a set of benchmarks (noise free benchmarks here)
