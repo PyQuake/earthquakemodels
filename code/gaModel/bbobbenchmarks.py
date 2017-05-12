9eda9f7f8af3c2d09d260e3ea468954a19fbc88c
yclavinas <yclavinas@gmail.com>
2017-05-12 12:23:07 -0300

    logging is not work properly

--- code/gaModel/bbobbenchmarks.py
+++ code/gaModel/bbobbenchmarks.py
@@ -94,7 +94,7 @@ from numpy import dot, linspace, diag, tile, zeros, sign, resize
 from numpy.random import standard_normal as _randn # TODO: may bring confusion
 from numpy.random import random as _rand # TODO: may bring confusion
 import sys
-sys.path.insert(0, '../../../')
+sys.path.insert(0, '../')
 from csep.loglikelihood import calcLogLikelihood
 from models.mathUtil import calcNumberBins
 import models.model
