6d595b1541c827b2d729acf710f7c6cedc936bc0
yclavinas <yclavinas@gmail.com>
2017-05-15 20:44:11 -0300

    testing exp

--- code/gaModel/gamodel_bbob.py
+++ code/gaModel/gamodel_bbob.py
@@ -57,10 +57,10 @@ def gaModel(func,NGEN,CXPB,MUTPB,modelOmega,year,region, mean, n_aval, tournsize
 	# Attribute generator
 	toolbox.register("evaluate", func, modelOmega = modelOmega, mean=mean)	
 	teste = toolbox.evaluate
-	@decorator(modelOmega, mean=None)
-	def aux(individual):
-		return teste(individual)
-	toolbox.register("evaluate", aux)
+	@decorator(modelOmega = modelOmega, mean=mean)	
+	def aux(individual, modelOmega = modelOmega, mean=mean):
+		return teste(individual, modelOmega = modelOmega, mean=mean)
+	toolbox.register("evaluate", aux, modelOmega = modelOmega, mean=mean)
 	# toolbox.register("evaluate", func, modelOmega = modelOmega, mean=mean)	
 	# toolbox.decorate("evaluate", tupleize)
 
