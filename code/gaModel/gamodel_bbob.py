404274544a72743bd4202fd5a768ed8c9149585a
yclavinas <yclavinas@gmail.com>
2017-05-15 18:46:43 -0300

    testing exp

--- code/gaModel/gamodel_bbob.py
+++ code/gaModel/gamodel_bbob.py
@@ -27,7 +27,7 @@ def tupleize(func):
     when the evaluation function returns a single value.
     """
     def wrapper(*args, **kargs):
-    	print(*args)
+    	print(args)
     	exit()
         return func(*args, **kargs),
     return wrapper
