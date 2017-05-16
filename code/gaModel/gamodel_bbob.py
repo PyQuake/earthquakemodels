6d733c6b688309ea189b336a21ccb79bbd740360
yclavinas <yclavinas@gmail.com>
2017-05-16 14:46:10 -0300

    prepraring serial execution

--- code/gaModel/gamodel_bbob.py
+++ code/gaModel/gamodel_bbob.py
@@ -128,6 +128,7 @@ if __name__ == "__main__":
 			continue
 		tokens = line.split()
 		for key,value in zip(keys,tokens):
+			print(key)
 			if key == 'CXPB' or key == 'MUTPB':
 				params[key] = float(value)
 			else:
