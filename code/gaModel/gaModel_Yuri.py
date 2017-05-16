bec521d95547d210a0dcb49b13f9a65e806dce6f
yclavinas <yclavinas@gmail.com>
2017-05-16 16:35:03 -0300

    prepraring serial execution

--- code/gaModel/gaModel_Yuri.py
+++ code/gaModel/gaModel_Yuri.py
@@ -83,6 +83,8 @@ def gaModel(NGEN,CXPB,MUTPB,modelOmega,year,region, mean, tournsize=2, n_aval=50
 	for ind, fit in zip(pop, fitnesses):
 		ind.fitness.values = fit
 	for g in range(NGEN):
+		if g%10 == 0:
+			print(g)
 		# Select the next generation individuals
 		offspring = toolbox.select(pop, len(pop))
 		# Clone the selected individuals
