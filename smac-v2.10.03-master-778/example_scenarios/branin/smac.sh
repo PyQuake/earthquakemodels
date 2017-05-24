be17f483f2804b8b7093d8e68b5a034e1f5101a4
yclavinas <yclavinas@gmail.com>
2017-05-24 16:54:31 -0300

    trying smac

--- smac-v2.10.03-master-778/example_scenarios/branin/smac.sh
+++ smac-v2.10.03-master-778/example_scenarios/branin/smac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # nohup python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006 &
-python gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006
+python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006
 
