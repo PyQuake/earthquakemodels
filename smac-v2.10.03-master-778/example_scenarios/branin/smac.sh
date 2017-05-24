cb3034f8cfb57eb124516d53fbbd962efdc299d0
yclavinas <yclavinas@gmail.com>
2017-05-24 16:57:48 -0300

    trying smac

--- smac-v2.10.03-master-778/example_scenarios/branin/smac.sh
+++ smac-v2.10.03-master-778/example_scenarios/branin/smac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # nohup python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006 &
-python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006
+# python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006
+./smac --scenario-file example_scenarios/branin/gaTest-scenario.txt 
 
