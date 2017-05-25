efacd314508e1293427e403471a22c749bacf3de
yclavinas <yclavinas@gmail.com>
2017-05-25 12:35:19 -0300

    trying smac

--- smac-v2.10.03-master-778/exp/smac-v2.10.03-master-778/smac.sh
+++ smac-v2.10.03-master-778/exp/smac-v2.10.03-master-778/smac.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# pip install git+https://github.com/DEAP/deap
-# pip install numpy
-# pip install pymysql
-./smac --scenario-file gaTest-scenario.txt 
-
+pip install git+https://github.com/DEAP/deap
+pip install numpy
+pip install pymysql
+./smac --scenario-file algorithm/gaTest-scenario.txt 
