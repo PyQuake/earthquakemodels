0ee4e648fcae3dd432747b797b19b92bdeb44958
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2017-09-02 19:20:26 -0300

    tidy data

---
+++ result_exp_benchmark/results_UniformGaussian20D-cleaner.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+i = 0
+for file in os.listdir("UniformGaussian20D"):
+	if file.endswith(".txt") and file.startswith("f"):
+		
+		f = open("UniformGaussian40D/"+file, "r")
+		if i == 0:
+			output_file = re.sub ('_[0-9]*.txt', '.txt', file)
+			g = open("results_UniformGaussian20D/"+output_file, "w")
+		# f.readline()
+		i += 1
+		for line in f:
+			data = line.split()
+
+			if data[0] == 'gen':
+				if i == 0:
+					data.append('exp_number')
+					output = ', '.join(data)
+					g.write(output)
+					g.write('\n')
+				exp_number = i 
+			elif i == 40:
+				i = 0
+			elif len(data) == 5:
+				data.append(exp_number)
+				output = ', '.join(str(e) for e in data)
+				g.write(output)
+				g.write('\n')
+		f.close()
+	
