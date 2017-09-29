668de999841181992f0613e418c1cfb71217066d
root <root@cluster0.local>
2017-09-28 21:18:42 -0300

    getting ready for exp

--- smac-v2.10.03-master-778/example_scenarios/branin/smac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# nohup python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006 &
-# python2.7 gamodel_bbob.py output ../../../params/gaParams.txt EastJapan 2006
-pip2.7 install git+https://github.com/DEAP/deap
-pip2.7 install numpy
-pip2.7 install pymysql
-./smac --scenario-file example_scenarios/branin/gaTest-scenario.txt 
-
